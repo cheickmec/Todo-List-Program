1a81254572000867930727c32d0a82e5e34bdb3a
cheickmec <cab25004@vt.edu>
2015-08-27 04:46:35 -0400

    Has strange output to file

--- main.cpp
+++ main.cpp
@@ -1,16 +1,21 @@
-#include<ifstream>
 #include <iostream>
+#include <fstream>
 
+using namespace std;
 int main(int argc, char** argv)
 {
+	cout << "The number of argments is"<<argc<<endl;
+	for(int i = 0; i < argc; i++){
+cout<<"Argument number "<<i<<" is :"<<argv[i]<<endl;
+} 
 	if(argc >= 3) //if no file name was 
 	{
 		char* fileName = argv[1]; 
-		std::ifstream inFile; //input file handler
-		inFile.open(fileName);	//open input file
+		ofstream inFile(fileName);	//open input file
 		char* task = argv[2];	//task description handler
-		while(task){
+		for(int i =2; i<argc;i++){
 			inFile << task; //output task desription to file
+			inFile<<" ";	//put space
 			task++;	//move to next string
 		}
 	
