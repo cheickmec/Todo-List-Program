7e26b3d9793d4b8cc2a7da7ce261aad997ad4301
cheickmec <cab25004@vt.edu>
2015-08-28 23:06:21 -0400

    first working version

--- include/Task.h
+++ include/Task.h
@@ -4,6 +4,7 @@
 #include<vector>
 #include<fstream>
 #include <iostream>
+#include <cstring>
 
 class Task
 {
@@ -20,23 +21,24 @@ class Todo
 {
     public:
         Todo();
+        unsigned int size(){return m_list.size();}
+        Task& operator[](unsigned int pos){return m_list[pos];}
+        unsigned int numb_element();
         void addToList(Task t);
         void parse(std::string filename);
         bool isEmpty();
+        void display();
+        void rewriteFile(std::string filename);
+        bool check(std::string str);
     private:
         std::vector<Task> m_list; //holds a list of all tasks
 };
-//Returns true if str has proper format
 bool isFormat(std::string str);
 bool isNumber(const char n);
-void process(std::string command, std::string filename, unsigned int argnumb,std::vector<std::string> alist);
-//to call if 2 arguments
-void process_2(std::string command, std::string filename, unsigned int argnumb,std::vector<std::string> alist);
-//to call if 3 arguments
-void process_3(std::string command, std::string filename, unsigned int argnumb,std::vector<std::string> alist);
-//to call if 4 arguments
-void process_4(std::string command, std::string filename, unsigned int argnumb,std::vector<std::string> alist);
-//to call if 5 or more arguments
-void process_5(std::string command, std::string filename, unsigned int argnumb,std::vector<std::string> alist);
+bool isNumber(std::string str);
+bool doMatch(std::string st1, std::string st2);
+bool isDigit(const char x);
+unsigned int getpos(std::string str);
+bool isJustaNumber(std::string str);
 #endif // TASK_H
 
